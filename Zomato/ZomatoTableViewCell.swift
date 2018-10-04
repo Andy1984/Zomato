d88870283f7d7bffcd43b79495c0ec505dd79190
yang.weicheng <445919439@qq.com>
2018-10-04 18:03:14 +1000

    增加阴影

--- Zomato/ZomatoTableViewCell.swift
+++ Zomato/ZomatoTableViewCell.swift
@@ -14,22 +14,39 @@ class ZomatoTableViewCell: UITableViewCell {
     var backgroundImageView:UIImageView!
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        nameLabel = UILabel(frame: CGRect(x: 10, y: cellHeight - 50, width: cellWidth, height: 30))
-        nameLabel.font = UIFont.systemFont(ofSize: 16)
-        nameLabel.textColor = .white
-        
-        addressLabel = UILabel(frame: CGRect(x: 10, y: cellHeight - 30, width: cellWidth, height: 30))
-        addressLabel.font = UIFont.systemFont(ofSize: 10)
-        addressLabel.textColor = .white
         
         backgroundImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: cellWidth, height: cellHeight))
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.clipsToBounds = true
         addSubview(backgroundImageView)
+        addGradient()
+        
+        nameLabel = UILabel(frame: CGRect(x: 10, y: cellHeight - 50, width: cellWidth, height: 30))
+        nameLabel.font = UIFont.systemFont(ofSize: 16)
+        nameLabel.textColor = .white
         addSubview(nameLabel)
+        
+        addressLabel = UILabel(frame: CGRect(x: 10, y: cellHeight - 30, width: cellWidth, height: 30))
+        addressLabel.font = UIFont.systemFont(ofSize: 10)
+        addressLabel.textColor = .white
         addSubview(addressLabel)
         
         
+        
+        
+        
+    }
+    
+    func addGradient() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: cellWidth, height: cellHeight))
+        let gradient = CAGradientLayer()
+        
+        gradient.frame = view.bounds
+        
+        gradient.colors = [UIColor(white: 0, alpha: 0.0).cgColor, UIColor(white: 0, alpha: 0.7).cgColor]
+        
+        view.layer.insertSublayer(gradient, at: 0)
+        addSubview(view)
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
